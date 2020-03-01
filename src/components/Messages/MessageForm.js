867e4853ea23be03d466939b8efc89c166d8eead
Faif <faif@pikselan.com>
2020-03-01 10:16:13 +0700

    set message component, text and media. add search

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import uuidv4 from 'uuid/v4';
 import firebase from '../../firebase';
 import { Segment, Button, Input } from 'semantic-ui-react';
+
+import FileModal from './FileModal';
+import ProgressBar from './ProgressBar';
 
 class MessageForm extends React.Component {
     state = {
@@ -8,14 +12,23 @@ class MessageForm extends React.Component {
         loading: false,
         errors: [],
         channel: this.props.currentChannel,
-        user: this.props.currentUser
+        user: this.props.currentUser,
+        modal: false,
+        uploadState: '',
+        uploadTask: null,
+        storageRef: firebase.storage().ref(),
+        percentUploaded: 0,
     }
+
+    openModal = () => this.setState({ modal: true });
+
+    closeModal = () => this.setState({ modal: false });
 
     handleChange = event => {
         this.setState({ [event.target.name]: event.target.value })
     }
 
-    createMessage = () => {
+    createMessage = (fileUrl = null) => {
         const message = {
             timestamp: firebase.database.ServerValue.TIMESTAMP,
             user: {
@@ -23,9 +36,13 @@ class MessageForm extends React.Component {
                 name: this.state.user.displayName,
                 avatar: this.state.user.photoURL
 
-            },
-            content: this.state.message
+            }
         };
+        if(fileUrl !== null) {
+            message['image'] = fileUrl;
+        } else {
+            message['content'] = this.state.message;
+        }
         return message;
     }
 
@@ -56,8 +73,63 @@ class MessageForm extends React.Component {
         }
     }
 
+    uploadFile = (file, metadata) => {
+        const pathToUpload = this.state.channel.id;
+        const ref = this.props.messagesRef;
+        const filePath = `chat/public/${uuidv4()}.jpg`;
+
+        this.setState({
+            uploadState: 'uploading',
+            uploadTask: this.state.storageRef.child(filePath).put(file, metadata)
+        },
+            () => {
+                this.state.uploadTask.on('state_changed', snap => {
+                    const percentUploaded = Math.round((snap.bytesTransferred / snap.totalBytes) * 100);
+                    this.setState({ percentUploaded });
+                },
+                    err => {
+                        console.error(err);
+                        this.setState({
+                            errors: this.state.errors.concat(err),
+                            uploadState: 'error',
+                            uploadTask: null
+                        })
+                    },
+                    () => {
+                        this.state.uploadTask.snapshot.ref.getDownloadURL().then(downloadUrl => {
+                             this.sendFileMessage(downloadUrl, ref, pathToUpload);
+                        })
+                        .catch(err => {
+                            console.error(err);
+                            this.setState({
+                                errors: this.state.errors.concat(err),
+                                uploadState: 'error',
+                                uploadTask: null
+                            })
+                        })
+                    }
+                )
+            }
+        )
+    }
+
+    sendFileMessage = (fileUrl, ref, pathToUpload) => {
+        ref.child(pathToUpload)
+            .push()
+            .set(this.createMessage(fileUrl))
+            .then(() => {
+                this.setState({ uploadState: 'done' })
+            })
+            .catch(err => {
+                console.error(err);
+                this.setState({
+                    errors: this.state.errors.concat(err)
+                })
+            })
+    }
+
     render() {
-        const { errors, message, loading } = this.state;
+        const { errors, message, loading, modal, uploadState, percentUploaded } = this.state;
         return (
             <Segment className="massage__form">
                 <Input
@@ -82,12 +154,20 @@ class MessageForm extends React.Component {
                         labelPosition="left"
                         icon="edit" />
                     <Button
-                        onClick={this.uploadMedia}
+                        onClick={this.openModal}
+                        disabled={uploadState === "uploading"}
                         color="teal"
                         content="Upload Media"
                         labelPosition="right"
                         icon="cloud upload" />
                 </Button.Group>
+                <FileModal 
+                    modal={modal}
+                    closeModal={this.closeModal}
+                    uploadFile={this.uploadFile} />
+                <ProgressBar
+                    uploadState={uploadState}
+                    percentUploaded={percentUploaded} />
             </Segment>
         )
     }
--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -12,7 +12,11 @@ class Messages extends React.Component {
         channel: this.props.currentChannel,
         user: this.props.currentUser,
         messages: [],
-        messagesLoading: true
+        messagesLoading: true,
+        numUniqueUsers: '',
+        searchTerm: '',
+        searchLoading: false,
+        searchResults: []
     }
 
     componentDidMount() {
@@ -34,8 +38,41 @@ class Messages extends React.Component {
             this.setState({
                 messages: loadedMessages,
                 messagesLoading: false
-            })
+            });
+            this.countUniqueUsers(loadedMessages);
         })
+    }
+
+    handleSearchChange = event => {
+        this.setState({
+            searchTerm: event.target.value,
+            searchLoading: true
+        }, () => this.handleSearchMessages());
+    }
+
+    handleSearchMessages = () => {
+        const channelMessages = [...this.state.messages];
+        const regex = new RegExp(this.state.searchTerm, 'gi');
+        const searchResults = channelMessages.reduce((acc, message) => {
+            if ( (message.content && message.content.match(regex)) || message.user.name.match(regex)) {
+                acc.push(message);
+            }
+            return acc;
+        }, []);
+        this.setState({ searchResults });
+        setTimeout(() => this.setState({ searchLoading: false }), 1000);
+    }
+
+    countUniqueUsers = messages => {
+        const uniqueUsers = messages.reduce((acc, message) => {
+            if (!acc.includes(message.user.name)) {
+                acc.push(message.user.name);
+            }
+            return acc;
+        }, []);
+        const plural = uniqueUsers.length > 1 || uniqueUsers.length === 0;
+        const numUniqueUsers = `${uniqueUsers.length} user${plural ? "s" : ""}`;
+        this.setState({ numUniqueUsers });
     }
 
     displayMessages = messages => messages.length > 0 && messages.map(message => (
@@ -45,16 +82,22 @@ class Messages extends React.Component {
             user={this.state.user} />
     ))
 
+    displayChannelName = channel => channel ? `#${channel.name}` : '';
+
     render() {
-        const { messagesRef, channel, user, messages } = this.state;
+        const { messagesRef, channel, user, messages, numUniqueUsers, searchTerm, searchResults, searchLoading } = this.state;
 
         return (
             <React.Fragment>
-                <MessagesHeader />
+                <MessagesHeader
+                    channelName={this.displayChannelName(channel)}
+                    numUniqueUsers={numUniqueUsers}
+                    handleSearchChange={this.handleSearchChange}
+                    searchLoading={searchLoading} />
 
                 <Segment>
                     <Comment.Group className="messages">
-                        {this.displayMessages(messages)}
+                        {searchTerm ? this.displayMessages(searchResults) : this.displayMessages(messages)}
                     </Comment.Group>
                 </Segment>
 
